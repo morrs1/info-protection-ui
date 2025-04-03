d0e22b6838e51f691c8bcac6ae21de92fbc5e73b
morrs <grishkovea@gmail.com>
2025-04-04 00:28:08 +0300

    feat: добавил експорт для модулей jackson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module org.example.infoprotectionui {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
     requires static lombok;
+    requires com.fasterxml.jackson.databind;
 
 
     opens org.example.infoprotectionui to javafx.fxml;
     exports org.example.infoprotectionui;
     exports org.example.infoprotectionui.controllers;
     opens org.example.infoprotectionui.controllers to javafx.fxml;
+    exports org.example.infoprotectionui.entities to com.fasterxml.jackson.databind;
 }
